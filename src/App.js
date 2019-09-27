652caca284718e556d62456152b36ccf76b11abb
suoguangxu <suoguangxu@imooc.com>
2019-09-27 16:33:21 +0800

    添加文件搜索组件

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FileSearch from './components/FileSearch'
 
 function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
-        <div className="col-3 bg-red"><h1>this is left pennal</h1></div>
+        <div className="col-3 bg-red">
+          <FileSearch
+            placeholder="文档搜索"
+            onFileSearch={(keywords) => {window.alert(keywords)}}
+          />
+        </div>
         <div className="col-9 bg-primary"><h1>this is right pennal</h1></div>
       </div>
     </div>

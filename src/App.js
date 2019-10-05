899a26c15096c27bd43978d69cff1362ad372d44
suoguangxu <suoguangxu@imooc.com>
2019-10-05 13:27:49 +0800

    优化左侧列表样式

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FileSearch from './components/FileSearch';
 import FileList from './components/FileList';
 import BottomBtn from './components/FileListButton';
@@ -12,21 +12,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import filemock from './mock/defaultFiles'
 
 function App () {
+  const [files, setFiles] = useState(filemock);
+  const [activeFileID, setActiveFileId] = useState('');
+  const [openFileIDs, setOpenFileIDs] = useState([]);
+  const [unsavedFileIDs, setUnsavedFileIDs] = useState([]);
+  // 查找已打开的文件列表
+  const openFiles = openFileIDs.map(openID => {
+    return files.find(file => file.id === openID)
+  })
+  // 查找当前选中文件
+  const activeFile = files.find(file => file.id === activeFileID)
   return (
     <div className="App container-fluid px-0">
       <div className="row no-gutters">
-        <div className="col-3 bg-red">
+        {/* 左侧列表区域 */}
+        <div className="col-3 bg-red left-panel">
           <FileSearch
             placeholder="search"
             onFileSearch={(keywords) => { window.alert(keywords) }}
           />
           <FileList
-            files={filemock}
+            files={files}
             onFileClick={(id) => { console.log(id) }}
             onFileDelete={(id) => { console.log('delete' + id) }}
             onFileSave={(id, newname) => { console.log(id, newname) }}
           />
-          <div className="row no-gutters">
+          <div className="row no-gutters bottom-btn-group">
             <BottomBtn
               text="新建"
               icon="add"
@@ -41,20 +52,22 @@ function App () {
             />
           </div>
         </div>
+        {/* 右侧内容区域 */}
         <div className="col-9">
           <TabList
-            files={filemock}
-            activeId="1"
+            files={openFiles}
+            activeId='1'
             unsaveIds={['1', '2']}
-            onTabClick={(id) => {console.log(`tabid:${id}`)}}
-            onSaveTab={() => {}}
-            onCloseTab={(id) => {console.log(`closing: ${id}`)}}
+            onTabClick={(id) => { console.log(`tabid:${id}`) }}
+            onSaveTab={() => { }}
+            onCloseTab={(id) => { console.log(`closing: ${id}`) }}
           />
           <SimpleMDE
-            value={filemock[1].body}
+            value={activeFile && activeFile.body}
             onChange={newvalue => console.info(newvalue)}
             options={{
-              minHeight: window.innerHeight - 150 + 'px'
+              minHeight: "85vh",
+              status: false
             }}
           />
         </div>
